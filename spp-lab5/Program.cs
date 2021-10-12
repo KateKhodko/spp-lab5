98a2cdb38cdd17650dabefd0293d414f00027c05
KateKhodko <kottikate@gmail.com>
2021-10-12 23:33:40 +0300

    Initial commit

---
+++ spp-lab5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace spp_lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\developing\\projects\\С#\\Test\\Test\\bin\\Debug\\net5.0\\Test.dll";
+            
+            Assembly.LoadFrom(path).GetTypes()
+                .Where(type => type.IsPublic)
+                .OrderBy(type => type.FullName)
+                .ToList()
+                .ForEach(type => Console.WriteLine(type.FullName));
+        }
+    }
+}
